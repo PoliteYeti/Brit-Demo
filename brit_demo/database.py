8e347d2c670321e7b572595e5b3c686339661ca2
James Hudson <james.hudson@boughtbymany.com>
2022-11-20 10:24:43 +0000

    isort correction.

--- brit_demo/database.py
+++ brit_demo/database.py
@@ -1,10 +1,9 @@
 import os
 
+from dotenv import load_dotenv
 from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-
-from dotenv import load_dotenv
 
 load_dotenv()
 
